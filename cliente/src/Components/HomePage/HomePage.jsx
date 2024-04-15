ca84ff8d06704dabdeedbae30bff8a743d07ed38
Facundo <facundovs84@hotmail.com.ar>
2024-04-15 17:07:45 -0300

    Creacion de Seccion derecha de HomePage

--- cliente/src/Components/HomePage/HomePage.jsx
+++ cliente/src/Components/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import { Grid } from '@mui/material'
 import React from 'react'
 import { Navigation } from '../Navigation/Navigation'
 import { HomeSection } from '../HomeSection/HomeSection'
+import { Right } from '../Right/Right'
 
 export const HomePage = () => {
     return (
@@ -9,11 +10,11 @@ export const HomePage = () => {
             <Grid item xs={0} lg={2.5} className='hidden lg:block w-full relative'>
                 <Navigation/>    
             </Grid>
-            <Grid item xs={12} lg={6} className='hidden lg:block w-full relative'>
+            <Grid item xs={12} lg={6} className='px-5 lg:px-9 hidden lg:block w-full relative'>
                 <HomeSection/>
             </Grid>
             <Grid item xs={0} lg={3} className='hidden lg:block w-full relative'>
-                <p className='text-center'>Derecha</p>
+                <Right/>
             </Grid>
         </Grid>
     )
