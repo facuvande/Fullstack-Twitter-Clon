f034b69d29cc34affe2648b3341311862315ddf4
Facundo <facundovs84@hotmail.com.ar>
2024-04-14 23:50:13 -0300

    Configuracion react-router-dom

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,8 +1,16 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import { HomePage } from './Components/HomePage/HomePage.jsx';
+import { Auth } from './Components/Auth/Auth.jsx';
 
 function App() {
   return (
     <div className=''>
+
+      <Routes>
+        <Route path='/' element={true ? <HomePage/> : <Auth/>}/>
+      </Routes>
+
     </div>
   )
 }
