bd43ebb02a4f115908947d8a913a267d18beb367
Facundo <facundovs84@hotmail.com.ar>
2024-04-14 23:57:00 -0300

    Creacion de estructura grid de HomePage

--- cliente/src/Components/HomePage/HomePage.jsx
+++ cliente/src/Components/HomePage/HomePage.jsx
@@ -1,9 +1,18 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 export const HomePage = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <Grid container xs={12} className='px-5 lg:px-36 justify-between'>
+            <Grid item xs={0} lg={2.5} className='hidden lg:block w-full relative'>
+                <p className='text-center'>Izquierda</p>      
+            </Grid>
+            <Grid item xs={12} lg={6} className='hidden lg:block w-full relative'>
+                <p className='text-center'>Medio</p>
+            </Grid>
+            <Grid item xs={0} lg={3} className='hidden lg:block w-full relative'>
+                <p className='text-center'>Derecha</p>
+            </Grid>
+        </Grid>
     )
 }
